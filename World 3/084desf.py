3f3ba240ad06d485fabe182113f15ef901ff8f86
LAB-BIO-PC3 <jojosvd24@gmail.com>
2025-04-24 15:36:10 -0300

    teste review labbio

--- World 3/084desf.py
+++ World 3/084desf.py
@@ -1,4 +1,8 @@
 # nome e peso de varias pessoas | mostrar quantas pessoas foram cadastradas | lista com as pessoas mais pesadas | lista com as pessoas mais leves
+pessoas = 0
+pesadas = []
+leves = []
 while True:
     nome = input('Nome: ').strip()
     peso = float(input('Peso: '))
+# -->>
