df1ec38b7afa47732ac0aafa9b2a94f381709832
LAB-BIO-PC3 <jojosvd24@gmail.com>
2025-04-24 12:24:01 -0300

    084desf

---
+++ World 3/084desf.py
@@ -0,0 +1,4 @@
+# nome e peso de varias pessoas | mostrar quantas pessoas foram cadastradas | lista com as pessoas mais pesadas | lista com as pessoas mais leves
+while True:
+    nome = input('Nome: ').strip()
+    peso = float(input('Peso: '))
