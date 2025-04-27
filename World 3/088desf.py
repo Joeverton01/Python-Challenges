c94691c34a243e26ea31264301164bdc15d3b93c
Joeverton pctrab_casa <joevertonpaulo01@gmail.com>
2025-04-27 18:17:30 -0300

    088desf

--- World 3/088desf.py
+++ World 3/088desf.py
@@ -1,1 +1,10 @@
 # MEGA SENA
+print('-='*20)
+print('{:^40}'.format('JOGA NA MEGA SENA'))
+print('-='*20)
+quantidade = int(input('Digite quantos jogos serão sorteados: '))
+for jogadas in range(1, quantidade+1):
+    opcoes = []
+    for c in range(61):
+        opcoes.append(c)
+    print(opcoes)
